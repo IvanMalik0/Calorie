8b4f06d6543c55124d3c1f14f48ea64d1ccbd923
Ivan <ivanmalikoval@gmail.com>
2024-02-16 13:56:56 +0300

    Calculator localStorage, red border on \D input

--- js/script.js
+++ js/script.js
@@ -406,9 +406,19 @@ window.addEventListener('DOMContentLoaded', () => {
     // Calculator
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female',
-        height, weight, age,
-        ratio = 1.375;
+    let sex, height, weight, age, ratio;
+
+    if (localStorage.getItem('sex')) {
+      sex = localStorage.getItem('sex');
+    } else {
+      sex = 'female';
+    };
+
+    if (localStorage.getItem('ratio')) {
+      ratio = localStorage.getItem('ratio');
+    } else {
+      ratio = 1.375;
+    };
 
     function calTotal() {
       if (!sex || !height || !weight || !age || !ratio) {
@@ -432,8 +442,10 @@ window.addEventListener('DOMContentLoaded', () => {
         elem.addEventListener('click', (e) => {
           if (e.target.getAttribute('data-ratio')) {
             ratio = +e.target.getAttribute('data-ratio');
+            localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
           } else {
             sex = e.target.getAttribute('id');
+            localStorage.setItem('sex', e.target.getAttribute('id'));
           }
   
           elements.forEach(elem => {
@@ -454,6 +466,13 @@ window.addEventListener('DOMContentLoaded', () => {
       const input = document.querySelector(selector);
 
       input.addEventListener('input', () => {
+
+        if (input.value.match(/\D/g)) {
+          input.style.border = '1px solid red';
+        } else {
+          input.style.border = 'none';
+        };
+
         switch(input.getAttribute('id')) {
           case 'height':
                 height = +input.value;
